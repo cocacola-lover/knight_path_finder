be745d5436b99bf18b3ca50aee84bfbeb5cb367a
cocacola-lover <pegas620570@gmail.com>
2023-01-16 22:02:26 +0300

    Created test for pointers and board

---
+++ test/chess_pointer.test.ts
@@ -0,0 +1,22 @@
+import {ChessPointer, buildBoard} from './../dist/index';
+
+test("Build Board works properly", () => {
+    const board = buildBoard(10, 10);
+
+    expect(board.squares.length).toBe(10);
+    for (let i = 0; i < 10; i++) expect(board.squares[0].length).toBe(10);
+
+    expect(board.createPointer(0, 3)).toEqual(board.createPointer(0,3));
+    expect(board.createPointer(0, 3)).not.toBe(board.createPointer(0,3));
+});
+
+test("ChessPointer works properly", () => {
+    const board = buildBoard(10, 10);
+
+    const pointer = board.createPointer(0, 0);
+    expect(pointer.getNeighbours()).toEqual([
+        new ChessPointer(2, 1, board.squares),
+        new ChessPointer(1, 2, board.squares), 
+    ])
+});
+
