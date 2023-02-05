bf9b24fa82c3f5fc24513b5f9db56f5c24a7be0e
cocacola-lover <pegas620570@gmail.com>
2023-02-05 21:20:19 +0300

    Rewrote Board as a class

--- src/board.ts
+++ src/board.ts
@@ -1,32 +1,24 @@
 import ChessPointer from "./chess_pointer.js";
 import { Square } from "./interfaces.js";
 
-interface Board {
-    squares : Square[][],
-    createPointer(x:number, y : number): ChessPointer;
-}
-
-
 function createEmptySquare () : Square {
-    return {distanceFromStart : null,
-        shortestPath : null,
-        isPassable : true,
-        other : null} as Square;
+    
+    return {isPassable : true, weight : 1} as Square;
 }
 
-export default function buildBoard(width : number, length : number) : Board {
+export default class Board {
+    squares : Square[][];
 
-    const squares : Square[][] = [];
+    constructor(width : number, length : number) {
+        this.squares = [];
 
-    for (let i = 0; i < length; i++) {
-
-        squares.push([]);
-
-        for (let j = 0; j < width; j++) squares[i].push(createEmptySquare());
+        for (let i = 0; i < length; i++) {
+            this.squares.push([]);
+            for (let j = 0; j < width; j++) this.squares[i].push(createEmptySquare());
+        }
     }
 
-    const createPointer = (x : number, y: number) => new ChessPointer(x, y, squares);
-
-    return {squares, createPointer};
-
+    createPointer (x : number, y: number) {
+        return new ChessPointer(x, y, this.squares);
+    }
 }
