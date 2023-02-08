cee25040e025bcc63c0f11f620c4b1cebd9b97bb
cocacola-lover <pegas620570@gmail.com>
2023-02-08 13:32:35 +0300

    Rewritten test for new ChessPointer

--- test/all_alrorithms.test.js
+++ test/all_alrorithms.test.js
@@ -1,23 +1,21 @@
-import {Board, ChessPointer, SearchResult, deepFirstSearchIterator, dijkstraSearchIterator} from './../dist/index';
-
-const search = {deepFirstSearchIterator, dijkstraSearchIterator};
+import {Board, SearchResult, iterators} from './../dist/index';
 
 const canReach = (a, b, searchMethod) => {
     const board = new Board(10, 10);
 
-    const ite = searchMethod(board.createPointer(a[0],a[1]), board.createPointer(b[0], b[1]));
+    const ite = searchMethod(board.createBasicPointer(a[0],a[1]), board.createBasicPointer(b[0], b[1]));
     while((ite()).result === SearchResult.SearchContinues);
 
     expect(board.squares[b[0]][b[1]].distanceFromStart).not.toBe(undefined);
 
     let walkBack = board.squares[b[0]][b[1]];
     while(walkBack.shortestPath !== undefined) walkBack = walkBack.shortestPath.at();
-    expect(walkBack).toBe(board.createPointer(a[0],a[1]).at());
+    expect(walkBack).toBe(board.createBasicPointer(a[0],a[1]).at());
 }
 
 test("Testing if they can reach", () => {
     const points = [[0, 0], [3, 1], [5, 6], [3, 4], [5, 9], [1, 1]];
     for (let i = 0; i < points.length - 1; i++) {
-        Object.values(search).forEach((method) => canReach(points[i], points[i+1], method));
+        Object.values(iterators).forEach((method) => canReach(points[i], points[i+1], method));
     }
 })
--- test/chess_pointer.test.js
+++ test/chess_pointer.test.js
@@ -1,4 +1,4 @@
-import {ChessPointer, Board} from './../dist/index';
+import {pointers, Board} from './../dist/index';
 
 test("Build Board works properly", () => {
     const board = new Board(10, 10);
@@ -6,27 +6,27 @@ test("Build Board works properly", () => {
     expect(board.squares.length).toBe(10);
     for (let i = 0; i < 10; i++) expect(board.squares[0].length).toBe(10);
 
-    expect(board.createPointer(0, 3)).toEqual(board.createPointer(0,3));
-    expect(board.createPointer(0, 3)).not.toBe(board.createPointer(0,3));
+    expect(board.createKnightPointer(0, 3)).toEqual(board.createKnightPointer(0,3));
+    expect(board.createKnightPointer(0, 3)).not.toBe(board.createKnightPointer(0,3));
 });
 
 test("ChessPointer works properly", () => {
     const board = new Board(10, 10);
 
-    const pointer = board.createPointer(0, 0);
+    const pointer = board.createKnightPointer(0, 0);
     expect(pointer.getPassableNeighbours()).toEqual([
-        new ChessPointer(2, 1, board.squares),
-        new ChessPointer(1, 2, board.squares), 
+        new pointers.KnightPointer(2, 1, board.squares),
+        new pointers.KnightPointer(1, 2, board.squares), 
     ])
 
-    const neighbourSquares = board.createPointer(2, 1).getPassableNeighbours().map((e) => e.at());
+    const neighbourSquares = board.createKnightPointer(2, 1).getPassableNeighbours().map((e) => e.at());
     [
-        new ChessPointer(0, 0, board.squares),
-        new ChessPointer(0, 2, board.squares),
-        new ChessPointer(4, 0, board.squares),
-        new ChessPointer(4, 2, board.squares), 
-        new ChessPointer(3, 3, board.squares),
-        new ChessPointer(1, 3, board.squares), 
+        new pointers.KnightPointer(0, 0, board.squares),
+        new pointers.KnightPointer(0, 2, board.squares),
+        new pointers.KnightPointer(4, 0, board.squares),
+        new pointers.KnightPointer(4, 2, board.squares), 
+        new pointers.KnightPointer(3, 3, board.squares),
+        new pointers.KnightPointer(1, 3, board.squares), 
     ].forEach((pointer) => expect(neighbourSquares).toContain(pointer.at()))
     
 });
